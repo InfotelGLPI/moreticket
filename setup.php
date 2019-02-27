3acfae5d6692510d56472d54dc281f767fb0643a
Kevin HAINRY <39367117+keha35@users.noreply.github.com>
2019-02-27 11:55:17 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -92,7 +92,7 @@ function plugin_version_moreticket() {
 
    return [
       'name'           => __('More ticket', 'moreticket'),
-      'version'        => "1.4.4",
+      'version'        => "1.4.5",
       'author'         => "<a href='http://infotel.com/services/expertise-technique/glpi/'>Infotel</a>",
       'homepage'       => "https://github.com/InfotelGLPI/moreticket",
       'license'        => 'GPLv2+',
