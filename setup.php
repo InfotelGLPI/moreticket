fc777372397ba4026a4c956db7afe65bc9b21f31
THIVET Simon <simon.thivet@infotel.com>
2024-03-12 16:54:29 +0100

    New version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MORETICKET_VERSION', '1.7.3');
+define('PLUGIN_MORETICKET_VERSION', '1.7.4');
 
 if (!defined("PLUGIN_MORETICKET_DIR")) {
     define("PLUGIN_MORETICKET_DIR", Plugin::getPhpDir("moreticket"));
