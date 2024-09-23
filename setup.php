562ac2ae9d51d60c7a0797bb5ac6b766e36c0a49
ArthurMinfotel <arthur.mallet@infotel.com>
2024-09-23 14:40:19 +0200

    bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MORETICKET_VERSION', '1.7.4');
+define('PLUGIN_MORETICKET_VERSION', '1.7.5');
 
 if (!defined("PLUGIN_MORETICKET_DIR")) {
     define("PLUGIN_MORETICKET_DIR", Plugin::getPhpDir("moreticket"));
