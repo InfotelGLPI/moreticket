4740704d42495d2a50852e36d7fec5639034dea3
Thetsmr <thetsmr@gmail.com>
2025-09-23 14:12:17 +0200

    Update Github Scripts

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
+# Recherche tous les fichiers PHP récursivement
+find . -name '*.php' > php_files.list
 
-xgettext *.php */*.php --copyright-holder='MoreTicket Development Team' --package-name='GLPI - MoreTicket plugin' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
-	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
-	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+# Extraction avec xgettext
+xgettext --files-from=php_files.list \
+  --copyright-holder='MoreTicket Development Team' \
+  --package-name='MoreTicket - Accounts plugin' \
+  -o locales/glpi.pot \
+  -L PHP \
+  --add-comments=TRANS \
+  --from-code=UTF-8 \
+  --force-po \
+  --keyword=_n:1,2,4t \
+  --keyword=__s:1,2t \
+  --keyword=__:1,2t \
+  --keyword=_e:1,2t \
+  --keyword=_x:1c,2,3t \
+  --keyword=_ex:1c,2,3t \
+  --keyword=_nx:1c,2,3,5t \
+  --keyword=_sx:1c,2,3t
 
-
-
+# Nettoyage
+rm php_files.list
