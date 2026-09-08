674b04e9a14bb6e739379452124c4d12b8556322
Thetsmr <thetsmr@gmail.com>
2026-09-08 16:16:42 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -38,7 +38,7 @@ use GlpiPlugin\Moreticket\TicketFollowup;
 use GlpiPlugin\Moreticket\TicketTask;
 use GlpiPlugin\Moreticket\WaitingTicket;
 
-define('PLUGIN_MORETICKET_VERSION', '1.8.11');
+define('PLUGIN_MORETICKET_VERSION', '1.8.12');
 
 global $CFG_GLPI;
 
